164c3eded27b6ceedd7ff94f0becd0ecc4e78295
Gary Newport <Gary.Newport@knightfrank.com>
2019-01-11 12:18:19 +0000

    update

--- DbDeploy/Program.cs
+++ DbDeploy/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+using DbUp;
 
 namespace DbDeploy
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static int Main()
         {
+            var connectionString = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+
+            // Create Db if it doesn't exist
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .LogScriptOutput()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+
+#if DEBUG
+            Console.ReadLine();
+#endif
+            return 0;
         }
     }
 }
+
